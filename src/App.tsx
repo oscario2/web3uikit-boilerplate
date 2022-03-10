4a06b8a85aae84a1e7731a59d6ecea9e4ec2dfe3
oscario2 <oscarandersson550@gmail.com>
2022-03-10 04:14:11 -0800

    fix: node modules for v5

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { CryptoCards, Button } from 'web3uikit';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+    <>
+        <CryptoCards chain="ethereum" onClick={()=>{}} chainType={''} bgColor={''} />
+        <Button theme="primary" type="button">
+            Launch dApp
+        </Button>
+    </>
+);
 
 export default App;
